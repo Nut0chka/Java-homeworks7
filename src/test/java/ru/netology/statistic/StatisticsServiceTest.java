419cff2a33255cb3d352a741cc7eaf83cefba1ee
Anna Demidova <annad.va00@gmail.com>
2024-01-14 14:11:31 +0300

    Corrected test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -1,7 +1,7 @@
 package ru.netology.statistic;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class StatisticsServiceTest {
     @Test
@@ -10,6 +10,18 @@ public class StatisticsServiceTest {
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
         long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMaxIfFirstNotMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {4, 8, 7, 5, 6, 2, 3, 1, 16, 3, 1};
+        long expected = 16;
 
         long actual = service.findMax(incomesInBillions);
 
